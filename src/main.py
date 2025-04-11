d87f64a9003ac98bad5ea2debfaad8df04a096ea
任智文 <renzhiwen@renzhiwendeMacBook-Air.local>
2025-04-11 17:10:53 +0800

    feat: 项目相关功能开发

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from tools.file_tools import *
 from tools.project_tools import *
 from tools.corpus_tools import *
+from tools.common_tools import *
 from server import mcp
 
 if __name__ == '__main__':
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,8 @@
 import httpx
+from pydantic import Field
+import uuid
 
+from server import host, headers
 
 async def send_request(method, url, headers, params, data):
     try:
@@ -15,4 +18,46 @@ async def send_request(method, url, headers, params, data):
     except httpx.HTTPError as e:
         raise Exception(f"HTTP request failed: {str(e)}")
     except Exception as e:
+        print(e)
         raise Exception(f"An error occurred: {str(e)}")
+
+
+async def upload_file(
+    file_path: str = Field(..., description="文件路径"),
+    file_name: str = Field(..., description="文件名"),
+):
+    """
+    Name:
+        上传文件
+    Description:
+        上传文件
+    """
+    url = f"{host}/api/common/upload_big_file"
+    headers["x-request-id"] = f"mcp-{str(uuid.uuid4())}"
+    headers['x-language'] = 'zh-CN'
+    identification = str(uuid.uuid4())
+    data = {
+        "no": "0",
+        "identification": identification,
+    }
+
+    async with httpx.AsyncClient() as client:
+        resp = await client.request("POST", url, headers=headers, data=data, files={"file": (file_name, open(file_path, "rb"), "application/octet-stream")})
+        result = resp.json()
+        if resp.status_code != 200 or result.get("code") != "0":
+            raise Exception(f"API response error: {result.get('message', 'unkown error')}")
+
+        # 获取上传文件的url
+        url = f"{host}/api/common/merge_file"
+        headers["x-request-id"] = f"mcp-{str(uuid.uuid4())}"
+        headers['x-language'] = 'zh-CN'
+        data = {
+            "filename": file_name,
+            "identification": identification,
+        }
+        resp = await client.request("POST", url, headers=headers, data=data)
+        result = resp.json()
+        if resp.status_code != 200 or result.get("code") != "0":
+            raise Exception(f"API response error: {result.get('message', 'unkown error')}")
+        data = result.get("data")
+        return data
